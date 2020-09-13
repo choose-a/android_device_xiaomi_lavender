e3fe3b751c16ba03611485706fc924084806b8ce
Erik Castricum <git@cas-online.nl>
2020-09-13 10:47:25 +0000

    Add ANX camera
    replace snapdragon camera for snapdragon gallery
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>
    Change-Id: Ibed833500ca353e23e47b4acf70be70076b5d7e0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,9 @@ VENDOR_SECURITY_PATCH := 2020-01-01
 # WLAN MAC
 WLAN_MAC_SYMLINK := true
 
+# ANXCamera
+-include vendor/aeonax/ANXCamera/BoardConfigAnx.mk
+
 # Revert me!
 ifneq ($(BOARD_USE_ENFORCING_SELINUX),true)
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
